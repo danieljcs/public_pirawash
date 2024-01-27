fcf54e9d96e1949078e4203371a339da7b539353
Dan <danieljcs21@hotmail.es>
2024-01-27 16:48:39 -0500

    fff

--- FRONT/assets/custom.js
+++ FRONT/assets/custom.js
@@ -1,14 +1,16 @@
 function openClose(){
   console.log('RAWTEST')
-  if (document.body.classList.contains("menu-close")) {
+  if (document.body.classList.contains("menu-hide")) {
     // Si la tiene, removerla
-    document.body.classList.remove("menu-close");
+    document.body.classList.remove("menu-hide");
     document.body.classList.add("menu-open");
+    console.log("r1")
   }
 
   if (document.body.classList.contains("menu-open")) {
     // Si la tiene, removerla
     document.body.classList.remove("menu-open");
-    document.body.classList.add("menu-close");
+    document.body.classList.add("menu-hide");
+    console.log("r2")
   }
 }
