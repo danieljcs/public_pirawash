494ac630a4f14674c2468b4a5aaa120f08152a93
Dan <danieljcs21@hotmail.es>
2024-01-28 21:55:08 -0500

    j

--- FRONT/assets/custom.js
+++ FRONT/assets/custom.js
@@ -1,22 +1,21 @@
-window.onload = function(){
+// window.onload = function(){
 
-  console.log("El DOM ha sido cargado completamente. Ahora ejecuto el último script.");
-  // Agrega aquí el código de tu último script
-  document.body.classList.add("menu-open","vertical-overlay-menu");
+//   console.log("El DOM ha sido cargado completamente. Ahora ejecuto el último script.");
+//   // Agrega aquí el código de tu último script
+//   document.body.classList.add("menu-open","vertical-overlay-menu");
 
-};
+// };
 
 
 function openClose(){
   alert('RAWTEST')
+  alert(document.body.classList)
   if (document.body.classList.contains("menu-hide")) {
     // Si la tiene, removerla
     document.body.classList.remove("menu-hide");
     document.body.classList.add("menu-open");
     console.log("r1")
-  }
-
-  if (document.body.classList.contains("menu-open")) {
+  }else {
     // Si la tiene, removerla
     document.body.classList.remove("menu-open");
     document.body.classList.add("menu-hide");
