93e134f96ce5479b56db3826e6575337f906270e
Dan <danieljcs21@hotmail.es>
2024-01-28 22:19:48 -0500

    dd

--- FRONT/assets/custom.js
+++ FRONT/assets/custom.js
@@ -2,25 +2,19 @@
 
 function openClose(){
 
-  alert(document.body.classList.contains("menu-open"))
   if(!document.body.classList.contains("menu-open") && !document.body.classList.contains("menu-hide")){
     document.body.classList.add("menu-hide");
-    alert("RAW1")
   }
   if(document.body.classList.contains("vertical-compact-menu")){
     document.body.classList.remove("vertical-compact-menu");
     document.body.classList.add("vertical-overlay-menu");
-    alert("RAW2")
   }
 
   if (document.body.classList.contains("menu-hide")) {
     document.body.classList.remove("menu-hide");
     document.body.classList.add("menu-open");
-    alert("RAW3")
   }else {
     document.body.classList.remove("menu-open");
     document.body.classList.add("menu-hide");
-    alert("RAW4")
   }
-  alert(document.body.classList.contains("menu-open"))
 }
